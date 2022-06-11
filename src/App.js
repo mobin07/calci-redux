421c4709bc583bbe6e7c66a6b9f0218ba728f916
mobin07 <mmobinmemon@gmail.com>
2022-06-11 15:53:59 +0530

    calcic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import {useDispatch,useSelector} from "react-redux";
+import {add,substract,multiply,divide} from "./store/action"
 
 function App() {
+  const dispatch=useDispatch();
+  const count=useSelector(state=>(state.count));
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div >{count}</div>
+    <button onClick={()=>dispatch(add(10))}>add</button>
+    <button onClick={()=>dispatch(substract(5))}>substract</button>
+    <button onClick={()=>dispatch(multiply(5))}>Mul</button>
+    <button onClick={()=>dispatch(divide(2))}>Div</button>
     </div>
+
   );
 }
 
